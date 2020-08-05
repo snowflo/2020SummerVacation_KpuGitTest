8774dca0d07743076d8ee68ce3527dd4ba6526b5
snowflo <g175151@gmail.com>
2020-08-05 13:36:46 +0900

    for merge test

--- app/src/main/java/kr/ac/kpu/kpusummerwater/MainActivity.kt
+++ app/src/main/java/kr/ac/kpu/kpusummerwater/MainActivity.kt
@@ -12,7 +12,7 @@ import androidx.appcompat.app.AlertDialog
 import kotlinx.android.synthetic.main.activity_main.*
 
 
-//ppt ch10 레이아웃 include 방법 참조! change test
+//ppt ch10 레이아웃 include 방법 참조! change test go to merge
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
