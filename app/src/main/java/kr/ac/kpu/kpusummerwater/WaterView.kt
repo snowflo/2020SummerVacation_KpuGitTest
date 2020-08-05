7300cae1675d7bda3aa325cd87dd692883d19013
snowflo <g175151@gmail.com>
2020-08-05 15:43:11 +0900

    Drawer End
    Drawer -> click -> Activity
    
    No Fragment

--- app/src/main/java/kr/ac/kpu/kpusummerwater/WaterView.kt
+++ app/src/main/java/kr/ac/kpu/kpusummerwater/WaterView.kt
@@ -1,12 +1,14 @@
 package kr.ac.kpu.kpusummerwater
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
+import android.view.MenuItem
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
+import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
-import androidx.fragment.app.FragmentManager
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
@@ -15,56 +17,43 @@ import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.navigation.NavigationView
 import com.google.android.material.snackbar.Snackbar
-import kr.ac.kpu.kpusummerwater.ui.home.HomeFragment
+import kotlinx.android.synthetic.main.activity_water_view.*
+import kotlinx.android.synthetic.main.content_main.*
 
-//waterView snowflowar backup
-class WaterView : AppCompatActivity() {
-
-    val fragmentManager = supportFragmentManager
-
+//WaterViewWaterView
+class WaterView : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
     private lateinit var appBarConfiguration: AppBarConfiguration
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_water_view)
-        //툴바
+        main_navigationView.setNavigationItemSelectedListener(this) //navigation 리스너
+
+        //툴바 앱바 지정
         val toolbar: Toolbar = findViewById(R.id.toolbar)
         setSupportActionBar(toolbar)
-        //플로팅액션버튼
+        supportActionBar?.setHomeAsUpIndicator(R.drawable.ic_baseline_menu_24)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
         val fab: FloatingActionButton = findViewById(R.id.fab)
         fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show()
         }
-        val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
-        val navView: NavigationView = findViewById(R.id.nav_view)
-        val navController = findNavController(R.id.nav_host_fragment)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        appBarConfiguration = AppBarConfiguration(
-            setOf(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow
-            ), drawerLayout
-        )
-        setupActionBarWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)
-        //==========================================================================================
-        if (intent.hasExtra("WaterData")) { //안 옴
+
+        //데이터 들어오는지 체크
+        if (intent.hasExtra("WaterData")) {
 
             var SiTest:String? = null
             var DongTest:String? = null
+            
 
-            //데이터 들어옴
+            //데이터 들어오는지 체크
             var data = intent.getParcelableExtra<data>("WaterData")
             if (data != null) { //debug
-                SiTest = data.Si
-                DongTest = data.Dong
-                Toast.makeText(this, "시: ${SiTest}, 동: ${DongTest} ", Toast.LENGTH_SHORT).show()
-
-                //fragment
-                val fragmentTransaction = fragmentManager.beginTransaction()
-                fragmentTransaction.replace(R.id.nav_host_fragment, HomeFragment())
-                fragmentTransaction.commit()
+                siText.text = data.Si
+                dongText.text = data.Dong
+                Toast.makeText(this, "시: ${data.Si}, 동: ${data.Dong} ", Toast.LENGTH_SHORT).show()
             }
             else{
                 Toast.makeText(this, "전달된 내용이 없습니다", Toast.LENGTH_SHORT).show()
@@ -81,35 +70,33 @@ class WaterView : AppCompatActivity() {
         return true
     }
 
-    override fun onSupportNavigateUp(): Boolean {
-        val navController = findNavController(R.id.nav_host_fragment)
-        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            android.R.id.home->{
+                drawer_layout.openDrawer(GravityCompat.START)
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        val itemIntent = Intent(this, MainActivity::class.java)
+        when(item.itemId){
+            R.id.item1->startActivity(itemIntent)
+            R.id.item2-> Toast.makeText(this,"item2 clicked",Toast.LENGTH_SHORT).show()
+            R.id.item3-> Toast.makeText(this,"item3 clicked",Toast.LENGTH_SHORT).show()
+        }
+        return false
+    }
+
+    override fun onBackPressed() { //뒤로가기 처리
+        if(drawer_layout.isDrawerOpen(GravityCompat.START)){
+            drawer_layout.closeDrawers()
+            // 테스트를 위해 뒤로가기 버튼시 Toast 메시지
+            Toast.makeText(this,"back btn clicked",Toast.LENGTH_SHORT).show()
+        } else{
+            super.onBackPressed()
+        }
     }
 }
-/*
-class data2(var Si: String?, var Dong: String?) : Parcelable {
-    constructor(parcel: Parcel) : this(
-        parcel.readString(),
-        parcel.readString()) {
-    }
-
-    override fun writeToParcel(parcel: Parcel, flags: Int) {
-        parcel.writeString(Si)
-        parcel.writeString(Dong)
-    }
-
-    override fun describeContents(): Int {
-        return 0
-    }
-
-    companion object CREATOR : Parcelable.Creator<data> {
-        override fun createFromParcel(parcel: Parcel): data {
-            return data(parcel)
-        }
-
-        override fun newArray(size: Int): Array<data?> {
-            return arrayOfNulls(size)
-        }
-    }
-}
-*/
